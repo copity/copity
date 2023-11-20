4c9e4630e1040bf2d20e3067b5627909b788865b
hywax <a.hywax@gmail.com>
2023-11-20 20:08:57 +0500

    chore: add `@nuxt/content`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,21 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
+    '@nuxt/content',
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
   ],
+  content: {
+    documentDriven: true,
+    highlight: {
+      theme: {
+        dark: 'github-dark',
+        default: 'github-light',
+      },
+      preload: ['json', 'js', 'ts', 'html', 'css', 'vue', 'diff', 'shell', 'markdown', 'yaml', 'bash', 'ini'],
+    },
+    navigation: {
+      fields: ['icon', 'titleTemplate', 'header', 'main', 'aside', 'footer'],
+    },
+  },
 })
