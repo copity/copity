b8f1252bfe2aa5de4b53aff672eca7f41a366062
hywax <a.hywax@gmail.com>
2023-11-26 14:12:32 +0500

    chore: define App Config

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,14 @@
 export default defineAppConfig({
   copity: {
-    title: 'Copity',
+    socials: {
+      github: 'copity/copity',
+    },
+    footer: {
+      credits: {
+        icon: 'IconCopity',
+        text: 'Powered by Copity',
+        href: 'https://github.com/copity/copity',
+      },
+    },
   },
 })
