481d98e382569da1f4281306abdff1f42c2a9c36
hywax <a.hywax@gmail.com>
2023-11-28 16:46:58 +0500

    chore: create `LangSwitcher` component

---
+++ components/app/LangSwitcher.vue
@@ -0,0 +1,5 @@
+<template>
+  <button type="button" class="flex h-6 w-6 items-center justify-center rounded-md transition hover:bg-zinc-900/5 dark:hover:bg-white/5 dark:text-zinc-400">
+    <Icon name="uil:language" class="h-4 w-4" />
+  </button>
+</template>
