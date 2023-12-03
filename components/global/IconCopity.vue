dc5ea9a74b20e9a8277603d05d821b9217d1d4eb
hywax <a.hywax@gmail.com>
2023-12-03 17:20:04 +0500

    chore: create global component `IconCopity`

---
+++ components/global/IconCopity.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg viewBox="0 0 24 26.449" fill="none" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M24 20.896a13.21 13.21 0 0 1-10.775 5.555C5.921 26.45 0 20.529 0 13.225 0 5.921 5.921 0 13.225 0l6.82 13.225L24 20.896Z" fill="currentColor" fill-opacity=".5" /><path fill-rule="evenodd" clip-rule="evenodd" d="M20.045 13.225 24 5.555A13.207 13.207 0 0 0 13.225 0C5.921 0 0 5.921 0 13.225 0 20.529 5.921 26.45 13.225 26.45l6.82-13.225Z" fill="currentColor" fill-opacity=".5" /></svg>
+</template>
